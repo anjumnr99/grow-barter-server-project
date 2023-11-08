9480094a6722c1d416a5eb63f6eb72da9b05396b
anjumnr99 <anjumanowara99@gmail.com>
2023-11-08 17:31:52 +0600

    deleted a booking data from server is done

--- index.js
+++ index.js
@@ -16,14 +16,14 @@ app.use(cookieParser());
 app.use(cors({
   origin: ['http://localhost:5173'],
   credentials: true,
-  
+
 }));
 
 
 //middlewares
 
-const logger = async(req,res,next) =>{
-  console.log("called", req.hostname , req.originalUrl);
+const logger = async (req, res, next) => {
+  console.log("called", req.hostname, req.originalUrl);
   next();
 }
 
@@ -67,24 +67,25 @@ async function run() {
 
     // auth related API
 
-    app.post('/jwt',logger, async (req, res) => {
+    app.post('/jwt', logger, async (req, res) => {
       const user = req.body;
       console.log(user);
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
       res       // setting the token in cookie
-      .cookie('token', token, {  
-              httpOnly: true,
-              secure: true, 
-              sameSite : 'none'
-          })
-      .send({ success: true })
-  })
+        .cookie('token', token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: 'none'
+        })
+        .send({ success: true })
+    })
 
     app.get('/services', async (req, res) => {
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
       res.send(result)
     });
+
 
     app.get('/services/:id', async (req, res) => {
       const ids = req.params.id;
@@ -98,7 +99,7 @@ async function run() {
       const query = { _id: new ObjectId(id) }
       const result = await serviceCollection.deleteOne(query);
       res.send(result);
-  })
+    })
 
     app.post('/bookings', async (req, res) => {
       const booking = req.body;
@@ -107,92 +108,132 @@ async function run() {
       res.send(result)
     });
 
-    app.get('/bookings',logger,verifyToken, async (req, res) => {
+    app.get('/bookings', async (req, res) => {
       console.log(req.query.email);
       console.log('userInfo from the valid token', req.userInfo);
-      if(req.query?.email !== req.userInfo.email){
-          return res.status(403).send({message : "Forbidden Access"});
-      }
-  
+
+      // if (req.query?.email !== req.userInfo.email) {
+      //   return res.status(403).send({ message: "Forbidden Access" });
+      // }
+
       let query = {};
       if (req.query?.email) {
-          query = { 
-            user_email: req.query.email
-            
-          }
+        query = {
+          user_email: req.query.email
+
+        }
       }
       const result = await bookingCollection.find(query).toArray();
       res.send(result);
-  });
+    });
 
-    app.get('/pending-work',logger,verifyToken, async (req, res) => {
+    app.patch('/bookings/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedStatus = req.body;
+      console.log(updatedStatus);
+      const updateDoc = {
+        $set: {
+          service_status: updatedStatus.service_status
+        },
+      };
+      const result = await bookingCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
+
+    app.delete('/bookings/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await bookingCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    app.get('/pending-work', logger, verifyToken, async (req, res) => {
       console.log(req.query.email);
       console.log('userInfo from the valid token', req.userInfo);
-      if(req.query?.email !== req.userInfo.email){
-          return res.status(403).send({message : "Forbidden Access"});
+      if (req.query?.email !== req.userInfo.email) {
+        return res.status(403).send({ message: "Forbidden Access" });
       }
-  
+
       let query = {};
       if (req.query?.email) {
-          query = {
-            Service_Provider_Email: req.query.email
-          }
+        query = {
+          Service_Provider_Email: req.query.email
+        }
       }
 
       const options = {
         // Include only the `title` and `imdb` fields in the returned document
-        projection: { Service_Name: 1, Service_Image: 1, booking_date: 1, service_status: 1, Service_Description : 1,Service_Price : 1 },
-    };
-      const result = await bookingCollection.find(query,options).toArray();
+        projection: { Service_Name: 1, Service_Image: 1, booking_date: 1, service_status: 1, Service_Description: 1, Service_Price: 1 },
+      };
+      const result = await bookingCollection.find(query, options).toArray();
       res.send(result);
-  });
-    app.get('/my-services',logger,verifyToken, async (req, res) => {
+    });
+
+    app.get('/my-services', logger, verifyToken, async (req, res) => {
       console.log(req.query.email);
       console.log('userInfo from the valid token', req.userInfo);
-      if(req.query?.email !== req.userInfo.email){
-          return res.status(403).send({message : "Forbidden Access"});
+      if (req.query?.email !== req.userInfo.email) {
+        return res.status(403).send({ message: "Forbidden Access" });
       }
-  
+
       let query = {};
       if (req.query?.email) {
-          query = {
-            Service_Provider_Email: req.query.email
-          }
+        query = {
+          Service_Provider_Email: req.query.email
+        }
       }
 
-    //   const options = {
-    //     // Include only the `title` and `imdb` fields in the returned document
-    //     projection: { Service_Name: 1, Service_Image: 1, booking_date: 1, service_status: 1, Service_Description : 1,Service_Price : 1 },
-    // };
+      //   const options = {
+      //     // Include only the `title` and `imdb` fields in the returned document
+      //     projection: { Service_Name: 1, Service_Image: 1, booking_date: 1, service_status: 1, Service_Description : 1,Service_Price : 1 },
+      // };
       const result = await serviceCollection.find(query).toArray();
       res.send(result);
-  });
+    });
 
-  app.put('/my-services/update/:id', async(req,res)=>{
-    const ids = req.params.id;
-    const filter = {_id : new ObjectId(ids)};
-    const options = { upsert: true };
-    const updatedProduct = req.body;
-    const product ={
-        $set : {
-           
-           Service_Name :updatedProduct.Service_Name,
-           Service_Image :updatedProduct.Service_Image,
-           Service_Description :updatedProduct.Service_Description,
-           Service_Price :updatedProduct.Service_Price,
-           Service_Area :updatedProduct.Service_Area,
-           Service_Provider_Name :updatedProduct.Service_Provider_Name,
-           Service_Provider_Email :updatedProduct.Service_Provider_Email
-           
-       }
-    }
+    app.get('/provider/services', async (req, res) => {
+      console.log('provider email :', req.query);
 
-    const result = await serviceCollection.updateOne(filter,product,options);
-    res.send(result)
+      let query = {};
 
-})
+      if (req.query?.email) {
+        query = {
+          Service_Provider_Email: req.query.email
+        }
+      }
 
-  
+      const result = await serviceCollection.find(query).toArray();
+
+      res.send(result);
+    });
+
+
+    app.put('/my-services/update/:id', async (req, res) => {
+      const ids = req.params.id;
+      const filter = { _id: new ObjectId(ids) };
+      const options = { upsert: true };
+      const updatedProduct = req.body;
+      const product = {
+        $set: {
+
+          Service_Name: updatedProduct.Service_Name,
+          Service_Image: updatedProduct.Service_Image,
+          Service_Description: updatedProduct.Service_Description,
+          Service_Price: updatedProduct.Service_Price,
+          Service_Area: updatedProduct.Service_Area,
+          Service_Provider_Name: updatedProduct.Service_Provider_Name,
+          Service_Provider_Email: updatedProduct.Service_Provider_Email
+
+        }
+      }
+
+      const result = await serviceCollection.updateOne(filter, product, options);
+      res.send(result)
+
+    })
+
+
 
     app.post('/services', async (req, res) => {
       const newService = req.body;
